469815b2445b6b64b0096fc4ee7fdff7bd894abd
Pablo Jurado <pjurado85@gmail.com>
2017-04-07 13:09:52 -0500

    finish task #3

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ var priceTotal = arrItems.reduce(function (acc, item) {
 var averagePrice = priceTotal / arrItems.length
 console.log('The average price is ' + averagePrice)
 
-
 // array of items that cost between $14.00 and $18.00 USD
 var arrItems14and18 = arrItems.filter(function (item) {
   return (item.price >= 14 && item.price <= 18)
@@ -18,4 +17,19 @@ var arrItems14and18 = arrItems.filter(function (item) {
 console.log('Items that cost between $14.00 USD and $18.00 USD:')
 arrItems14and18.forEach(function (item) {
   console.log(item.title, item.price)
+})
+
+// item with a "GBP" currency code and print its name and price.
+var ItemCurrencyGBP = arrItems.filter(function (item) {
+  return item.currency_code === 'GBP'
+})
+console.log('Item Currency GBP', ItemCurrencyGBP)
+
+// items are made of wood.
+var itemsMadeOfWood = arrItems.filter(function (item) {
+  return item.materials.includes('wood')
+})
+console.log('Items made of wood')
+itemsMadeOfWood.forEach(function (item) {
+  console.log(item.title, item.materials)
 })
